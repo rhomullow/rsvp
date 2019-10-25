141dc50f5c1b87308d933af1e491670e2d32a1fd
Rômulo Silva <romulo.sm15@gmail.com>
2019-10-24 23:22:32 -0300

    Model e migrate

---
+++ app/Models/Site/Guest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Site;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Guest extends Model
+{
+    $fillable = ['name','answers'];
+}
---
+++ database/migrations/2019_10_25_021153_create_guests_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGuestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guests', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name',100)->nullable();
+            $table->integer('answers')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guests');
+    }
+}
