7bce3334cd4761a3bda53f67451ce47d8c41cf68
Rômulo Silva <romulo.sm15@gmail.com>
2019-10-25 23:49:14 -0300

    horario alterado

--- resources/views/one.blade.php
+++ resources/views/one.blade.php
@@ -10,7 +10,7 @@
 
 			<p>Só se atente ao detalhes, a trilha sonora é <br><strong class="highlight">emo e pop/punk dos anos 2000</strong>, você deverá <strong class="highlight">se vestir de preto</strong> e o <strong class="highlight">cardápio vai ser comida mexicana</strong>, mas não precisa se preocupar que tem comida sem pimenta também.</p> 
 
-			<p>Então te vejo no <strong>dia 02/11/2019 às 18h na <br>Rua Rafael de Aguiar, 31 casa 1, CEP 23587460.</strong></p>
+			<p>Então te vejo no <strong>dia 02/11/2019 às 20h na <br>Rua Rafael de Aguiar, 31 casa 1, CEP 23587460.</strong></p>
 
 			<p>
 				<strong class="highlight">xoxo <br> Tiffannyk</strong>
